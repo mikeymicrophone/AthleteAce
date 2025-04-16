cacc2e465818706ff180ef56afcf4db527a0ffad
Mike Schwab <mike.schwab@gmail.com>
2025-04-16 16:10:25 -0400

    tailwind

--- db/seeds/players/nba.rb
+++ db/seeds/players/nba.rb
@@ -2,8 +2,8 @@
 # This file seeds players for NBA playoff teams (as of the 2025 playoffs)
 # Includes comprehensive data for key teams, with structure to expand for others.
 
-nba_playoff_teams = {
-  "Boston Celtics" => [
+nba_playoff_teams = [
+  { territory: "Boston", mascot: "Celtics", players: [
     { first_name: "Jayson", last_name: "Tatum", nicknames: ["JT"], birthdate: "1998-03-03", jersey_number: 0, current_position: "SF", height_in: 80, weight_lb: 210, debut_year: 2017, draft_year: 2017, active: true },
     { first_name: "Jaylen", last_name: "Brown", nicknames: [], birthdate: "1996-10-24", jersey_number: 7, current_position: "SG", height_in: 78, weight_lb: 223, debut_year: 2016, draft_year: 2016, active: true },
     { first_name: "Kristaps", last_name: "Porzingis", nicknames: ["Unicorn"], birthdate: "1995-08-02", jersey_number: 8, current_position: "PF/C", height_in: 87, weight_lb: 240, debut_year: 2015, draft_year: 2015, active: true },
@@ -15,8 +15,8 @@ nba_playoff_teams = {
     { first_name: "Luke", last_name: "Kornet", nicknames: [], birthdate: "1995-07-15", jersey_number: 40, current_position: "C", height_in: 86, weight_lb: 250, debut_year: 2017, draft_year: 0, active: true },
     { first_name: "Neemias", last_name: "Queta", nicknames: [], birthdate: "1999-07-13", jersey_number: 88, current_position: "C", height_in: 84, weight_lb: 248, debut_year: 2021, draft_year: 2021, active: true }
     # Add more Celtics players as needed
-  ],
-  "Milwaukee Bucks" => [
+  ]},
+  { territory: "Milwaukee", mascot: "Bucks", players: [
     { first_name: "Giannis", last_name: "Antetokounmpo", nicknames: ["Greek Freak"], birthdate: "1994-12-06", jersey_number: 34, current_position: "PF", height_in: 83, weight_lb: 242, debut_year: 2013, draft_year: 2013, active: true },
     { first_name: "Damian", last_name: "Lillard", nicknames: ["Dame"], birthdate: "1990-07-15", jersey_number: 0, current_position: "PG", height_in: 74, weight_lb: 195, debut_year: 2012, draft_year: 2012, active: true },
     { first_name: "Khris", last_name: "Middleton", nicknames: [], birthdate: "1991-08-12", jersey_number: 22, current_position: "SF", height_in: 79, weight_lb: 222, debut_year: 2012, draft_year: 2012, active: true },
@@ -26,61 +26,61 @@ nba_playoff_teams = {
     { first_name: "Pat", last_name: "Connaughton", nicknames: [], birthdate: "1993-01-06", jersey_number: 24, current_position: "SG", height_in: 77, weight_lb: 209, debut_year: 2015, draft_year: 2015, active: true },
     { first_name: "Jae", last_name: "Crowder", nicknames: [], birthdate: "1990-07-06", jersey_number: 99, current_position: "SF", height_in: 78, weight_lb: 235, debut_year: 2012, draft_year: 2012, active: true }
     # Add more Bucks players as needed
-  ],
-  "New York Knicks" => [
+  ]},
+  { territory: "New York", mascot: "Knicks", players: [
     { first_name: "Jalen", last_name: "Brunson", nicknames: [], birthdate: "1996-08-31", jersey_number: 11, current_position: "PG", height_in: 74, weight_lb: 190, debut_year: 2018, draft_year: 2018, active: true },
     { first_name: "Julius", last_name: "Randle", nicknames: [], birthdate: "1994-11-29", jersey_number: 30, current_position: "PF", height_in: 80, weight_lb: 250, debut_year: 2014, draft_year: 2014, active: true },
     { first_name: "OG", last_name: "Anunoby", nicknames: [], birthdate: "1997-07-17", jersey_number: 8, current_position: "SF", height_in: 79, weight_lb: 232, debut_year: 2017, draft_year: 2017, active: true },
     { first_name: "Donte", last_name: "DiVincenzo", nicknames: ["Big Ragu"], birthdate: "1997-01-31", jersey_number: 0, current_position: "SG", height_in: 76, weight_lb: 203, debut_year: 2018, draft_year: 2018, active: true },
     { first_name: "Isaiah", last_name: "Hartenstein", nicknames: [], birthdate: "1998-05-05", jersey_number: 55, current_position: "C", height_in: 84, weight_lb: 250, debut_year: 2018, draft_year: 2017, active: true }
     # Add more Knicks players as needed
-  ],
-  "Cleveland Cavaliers" => [
+  ]},
+  { territory: "Cleveland", mascot: "Cavaliers", players: [
     { first_name: "Donovan", last_name: "Mitchell", nicknames: ["Spida"], birthdate: "1996-09-07", jersey_number: 45, current_position: "SG", height_in: 73, weight_lb: 215, debut_year: 2017, draft_year: 2017, active: true },
     { first_name: "Darius", last_name: "Garland", nicknames: [], birthdate: "2000-01-26", jersey_number: 10, current_position: "PG", height_in: 73, weight_lb: 192, debut_year: 2019, draft_year: 2019, active: true },
     { first_name: "Evan", last_name: "Mobley", nicknames: [], birthdate: "2001-06-18", jersey_number: 4, current_position: "PF", height_in: 83, weight_lb: 215, debut_year: 2021, draft_year: 2021, active: true },
     { first_name: "Jarrett", last_name: "Allen", nicknames: ["Fro"], birthdate: "1998-04-21", jersey_number: 31, current_position: "C", height_in: 81, weight_lb: 243, debut_year: 2017, draft_year: 2017, active: true }
     # Add more Cavaliers players as needed
-  ],
-  "Miami Heat" => [
+  ]},
+  { territory: "Miami", mascot: "Heat", players: [
     { first_name: "Jimmy", last_name: "Butler", nicknames: ["Jimmy Buckets"], birthdate: "1989-09-14", jersey_number: 22, current_position: "SF", height_in: 79, weight_lb: 230, debut_year: 2011, draft_year: 2011, active: true },
     { first_name: "Bam", last_name: "Adebayo", nicknames: [], birthdate: "1997-07-18", jersey_number: 13, current_position: "C", height_in: 81, weight_lb: 255, debut_year: 2017, draft_year: 2017, active: true },
     { first_name: "Tyler", last_name: "Herro", nicknames: ["Boy Wonder"], birthdate: "2000-01-20", jersey_number: 14, current_position: "SG", height_in: 77, weight_lb: 195, debut_year: 2019, draft_year: 2019, active: true }
     # Add more Heat players as needed
-  ],
-  "Philadelphia 76ers" => [
+  ]},
+  { territory: "Philadelphia", mascot: "76ers", players: [
     { first_name: "Joel", last_name: "Embiid", nicknames: ["The Process"], birthdate: "1994-03-16", jersey_number: 21, current_position: "C", height_in: 84, weight_lb: 280, debut_year: 2016, draft_year: 2014, active: true },
     { first_name: "Tyrese", last_name: "Maxey", nicknames: [], birthdate: "2000-11-04", jersey_number: 0, current_position: "PG", height_in: 74, weight_lb: 200, debut_year: 2020, draft_year: 2020, active: true },
     { first_name: "Tobias", last_name: "Harris", nicknames: [], birthdate: "1992-07-15", jersey_number: 12, current_position: "SF/PF", height_in: 80, weight_lb: 226, debut_year: 2011, draft_year: 2011, active: true }
     # Add more 76ers players as needed
-  ],
-  "Denver Nuggets" => [
+  ]},
+  { territory: "Denver", mascot: "Nuggets", players: [
     { first_name: "Nikola", last_name: "Jokic", nicknames: ["Joker"], birthdate: "1995-02-19", jersey_number: 15, current_position: "C", height_in: 83, weight_lb: 284, debut_year: 2015, draft_year: 2014, active: true },
     { first_name: "Jamal", last_name: "Murray", nicknames: ["Blue Arrow"], birthdate: "1997-02-23", jersey_number: 27, current_position: "PG", height_in: 76, weight_lb: 215, debut_year: 2016, draft_year: 2016, active: true }
     # Add more Nuggets players as needed
-  ],
-  "Minnesota Timberwolves" => [
+  ]},
+  { territory: "Minnesota", mascot: "Timberwolves", players: [
     { first_name: "Anthony", last_name: "Edwards", nicknames: ["Ant-Man"], birthdate: "2001-08-05", jersey_number: 5, current_position: "SG", height_in: 76, weight_lb: 225, debut_year: 2020, draft_year: 2020, active: true },
     { first_name: "Karl-Anthony", last_name: "Towns", nicknames: ["KAT"], birthdate: "1995-11-15", jersey_number: 32, current_position: "PF/C", height_in: 83, weight_lb: 248, debut_year: 2015, draft_year: 2015, active: true }
     # Add more Timberwolves players as needed
-  ],
-  "Oklahoma City Thunder" => [
+  ]},
+  { territory: "Oklahoma City", mascot: "Thunder", players: [
     { first_name: "Shai", last_name: "Gilgeous-Alexander", nicknames: ["SGA"], birthdate: "1998-07-12", jersey_number: 2, current_position: "PG", height_in: 78, weight_lb: 180, debut_year: 2018, draft_year: 2018, active: true },
     { first_name: "Chet", last_name: "Holmgren", nicknames: [], birthdate: "2002-05-01", jersey_number: 7, current_position: "PF/C", height_in: 85, weight_lb: 195, debut_year: 2023, draft_year: 2022, active: true }
     # Add more Thunder players as needed
-  ],
-  "Los Angeles Clippers" => [
+  ]},
+  { territory: "Los Angeles", mascot: "Clippers", players: [
     { first_name: "Kawhi", last_name: "Leonard", nicknames: ["The Claw"], birthdate: "1991-06-29", jersey_number: 2, current_position: "SF", height_in: 79, weight_lb: 225, debut_year: 2011, draft_year: 2011, active: true },
     { first_name: "Paul", last_name: "George", nicknames: ["PG-13"], birthdate: "1990-05-02", jersey_number: 13, current_position: "SF", height_in: 80, weight_lb: 220, debut_year: 2010, draft_year: 2010, active: true }
     # Add more Clippers players as needed
-  ]
+  ]}
   # Additional playoff teams can be added here (e.g., Phoenix Suns, Dallas Mavericks, Los Angeles Lakers, etc.)
-}
+]
 
-nba_playoff_teams.each do |team_name, players|
-  team = Team.find_by(territory: team_name.split.first, mascot: team_name.split.last)
+nba_playoff_teams.each do |team_data|
+  team = Team.find_by(territory: team_data[:territory], mascot: team_data[:mascot])
   if team
-    players.each do |attrs|
+    team_data[:players].each do |attrs|
       Player.find_or_create_by!(
         first_name: attrs[:first_name],
         last_name: attrs[:last_name],
@@ -96,9 +96,9 @@ nba_playoff_teams.each do |team_name, players|
         team: team
       )
     end
-    puts "Seeded players for #{team_name}"
+    puts "Seeded players for #{team_data[:territory]} #{team_data[:mascot]}"
   else
-    puts "Team #{team_name} not found"
+    puts "Team #{team_data[:territory]} #{team_data[:mascot]} not found"
   end
 end
 puts 'NBA playoff player seeding complete.'
