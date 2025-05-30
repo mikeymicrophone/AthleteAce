240580a4f8a0a2b72fa8974cf6ace12a5075e73e
Mike Schwab <mike.schwab@gmail.com>
2025-05-30 18:26:44 -0400

    turbo update for division game wip

--- app/controllers/division_guessing_games_controller.rb
+++ app/controllers/division_guessing_games_controller.rb
@@ -2,49 +2,18 @@ class DivisionGuessingGamesController < ApplicationController
   before_action :authenticate_ace! # Ensure user is logged in
 
   def new
-    # Allow difficulty to be configurable via params
-    difficulty = params[:difficulty]&.to_sym || :conference
-    # Validate difficulty is one of the allowed values
-    difficulty = :conference unless [:conference, :league].include?(difficulty)
-    
-    # Allow number of choices to be configurable
-    num_choices = params[:num_choices]&.to_i || 4
-    # Ensure num_choices is within a reasonable range
-    num_choices = 4 if num_choices < 2 || num_choices > 8
-
-    service = DivisionGameSetupService.new(difficulty: difficulty, num_choices: num_choices)
-    game_data = service.call
-
-    if game_data.nil? || game_data.team.nil? || game_data.correct_division.nil? || game_data.choices.blank? || game_data.choices.length < num_choices
-      # Provide more specific error message based on difficulty
-      error_message = "Could not set up a new game with #{difficulty} difficulty. "
-      error_message += case difficulty
-                      when :conference
-                        "There might not be enough divisions in the same conference and league. Try the 'League' difficulty instead."
-                      when :league
-                        "There might not be enough divisions in the same league. Please ensure your database has teams with active divisions."
-                      else
-                        "There might not be enough teams or divisions with the current settings. Please try again later."
-                      end
-      
-      flash[:alert] = error_message
-      redirect_to new_division_game_path # Redirect to the game page without parameters to show difficulty selection
-      return
+    setup_game
+    respond_to do |format|
+      format.html
+      format.turbo_stream
     end
+  end
 
-    @team = game_data.team
-    @correct_division = game_data.correct_division
-    @choices = game_data.choices # Array of Division objects
-    @sport = game_data.sport # Pass the sport to the view
-
-    # Store necessary info in session for the create action
-    session[:division_game_team_id] = @team.id
-    session[:division_game_correct_division_id] = @correct_division.id
-    session[:division_game_difficulty] = difficulty.to_s
-    session[:division_game_choice_ids] = @choices.map(&:id)
-    session[:division_game_start_time] = Time.current.to_f
-
-    # The view (new.html.erb) will display @team and @choices
+  def update
+    setup_game
+    respond_to do |format|
+      format.turbo_stream
+    end
   end
 
   def create
@@ -104,5 +73,53 @@ class DivisionGuessingGamesController < ApplicationController
     end
 
     redirect_to new_division_game_path
+  end
+
+  private
+
+  def setup_game
+    # Allow difficulty to be configurable via params
+    difficulty = params[:difficulty]&.to_sym || :conference
+    # Validate difficulty is one of the allowed values
+    difficulty = :conference unless [:conference, :league].include?(difficulty)
+    
+    # Allow number of choices to be configurable
+    num_choices = params[:num_choices]&.to_i || 4
+    # Ensure num_choices is within a reasonable range
+    num_choices = 4 if num_choices < 2 || num_choices > 8
+
+    service = DivisionGameSetupService.new(difficulty: difficulty, num_choices: num_choices)
+    game_data = service.call
+
+    if game_data.nil? || game_data.team.nil? || game_data.correct_division.nil? || game_data.choices.blank? || game_data.choices.length < num_choices
+      # Provide more specific error message based on difficulty
+      error_message = "Could not set up a new game with #{difficulty} difficulty. "
+      error_message += case difficulty
+                      when :conference
+                        "There might not be enough divisions in the same conference and league. Try the 'League' difficulty instead."
+                      when :league
+                        "There might not be enough divisions in the same league. Please ensure your database has teams with active divisions."
+                      else
+                        "There might not be enough teams or divisions with the current settings. Please try again later."
+                      end
+      
+      flash[:alert] = error_message
+      redirect_to new_division_game_path # Redirect to the game page without parameters to show difficulty selection
+      return
+    end
+
+    @team = game_data.team
+    @correct_division = game_data.correct_division
+    @choices = game_data.choices # Array of Division objects
+    @sport = game_data.sport # Pass the sport to the view
+
+    # Store necessary info in session for the create action
+    session[:division_game_team_id] = @team.id
+    session[:division_game_correct_division_id] = @correct_division.id
+    session[:division_game_difficulty] = difficulty.to_s
+    session[:division_game_choice_ids] = @choices.map(&:id)
+    session[:division_game_start_time] = Time.current.to_f
+
+    # The view (new.html.erb) will display @team and @choices
   end
 end
