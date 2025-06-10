fc289b1f52cdf906a0942442dde9edd6ce9417dd
Mike Schwab <mike.schwab@gmail.com>
2025-06-10 13:40:49 -0400

    wipe out round 1

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -7,16 +7,16 @@ module QuestsHelper
     existing_goal = current_ace.goals.find_by(quest: quest)
     
     if existing_goal
-      link_to quest_path(quest), class: "rounded-md px-3.5 py-2 bg-green-600 hover:bg-green-500 text-white font-medium #{options[:class]}" do
-        content_tag(:span, class: 'flex items-center') do
-          content_tag(:i, nil, class: 'fa-solid fa-check mr-2') + 
+      link_to quest_path(quest), class: "quest-continue-button #{options[:class]}" do
+        content_tag(:span, class: 'quest-button-content') do
+          content_tag(:i, nil, class: 'fa-solid fa-check quest-button-icon') + 
           content_tag(:span, 'Continue Quest')
         end
       end
     else
-      button_to quest_goals_path(quest), method: :post, class: "rounded-md px-3.5 py-2 bg-blue-600 hover:bg-blue-500 text-white font-medium #{options[:class]}" do
-        content_tag(:span, class: 'flex items-center') do
-          content_tag(:i, nil, class: 'fa-solid fa-flag mr-2') + 
+      button_to quest_goals_path(quest), method: :post, class: "quest-begin-button #{options[:class]}" do
+        content_tag(:span, class: 'quest-button-content') do
+          content_tag(:i, nil, class: 'fa-solid fa-flag quest-button-icon') + 
           content_tag(:span, 'Begin Quest')
         end
       end
@@ -32,8 +32,8 @@ module QuestsHelper
   # Displays the number of participants on a quest with appropriate styling
   def quest_participants_badge(quest)
     count = quest_participants_count(quest)
-    content_tag(:span, class: 'inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-indigo-100 text-indigo-800') do
-      content_tag(:i, nil, class: 'fa-solid fa-users mr-1') + 
+    content_tag(:span, class: 'quest-participants-badge') do
+      content_tag(:i, nil, class: 'fa-solid fa-users quest-participants-icon') + 
       pluralize(count, 'participant')
     end
   end
